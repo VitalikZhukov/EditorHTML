b97663026dbd41d4ffefe0fd865ad45b2952e209
Vitalik Zhukau <unicraft1989@gmail.com>
2021-07-24 19:04:48 +0300

    add methodes getPlainText and setPlainText to Controller

--- src/Controller.java
+++ src/Controller.java
@@ -11,6 +11,14 @@ public class Controller {
     public Controller(View view) {
         this.view = view;
     }
+
+    public static void main(String[] args) {
+        View view = new View();
+        Controller controller = new Controller(view);
+        view.setController(controller);
+        view.init();
+        controller.init();
+    }
     
     public HTMLDocument getDocument() {
         return document;
@@ -25,13 +33,29 @@ public class Controller {
         document.addUndoableEditListener(view.getUndoListener());
         view.update();
     }
+    
+    //записывает переданный text с html в document
+    public void setPlainText(String text) {
+        resetDocument();
+        StringReader reader = new StringReader(text);
+        HTMLEditorKit htmlEditorKit = new HTMLEditorKit();
+        try {
+            htmlEditorKit.read(reader, document, 0);
+        } catch (IOException | BadLocationException e) {
+            ExceptionHandler.log(e);
+        }
+    }
 
-    public static void main(String[] args) {
-        View view = new View();
-        Controller controller = new Controller(view);
-        view.setController(controller);
-        view.init();
-        controller.init();
+    //возвращает текст с html
+    public String getPlainText() {
+        StringWriter writer = new StringWriter();
+        HTMLEditorKit htmlEditorKit = new HTMLEditorKit();
+        try {
+            htmlEditorKit.write(writer, document, 0, document.getLength());
+        } catch (IOException | BadLocationException e) {
+            ExceptionHandler.log(e);
+        }
+        return writer.toString();
     }
 
     //отвечает за инициализацию view

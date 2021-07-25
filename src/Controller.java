335556752d22b6a31d1e67024ba45499aa111ded
Vitalik Zhukau <unicraft1989@gmail.com>
2021-07-25 19:18:10 +0300

    fill methods openDocument and saveDocument in Controller, add revesion for the future

--- src/Controller.java
+++ src/Controller.java
@@ -1,12 +1,10 @@
 package com.javarush.task.task32.task3209;
 
+import javax.swing.*;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.html.HTMLDocument;
 import javax.swing.text.html.HTMLEditorKit;
-import java.io.File;
-import java.io.IOException;
-import java.io.StringReader;
-import java.io.StringWriter;
+import java.io.*;
 
 public class Controller {
     private View view; //визуализация
@@ -75,9 +73,39 @@ public class Controller {
         currentFile = null;
     }
 
-    public void openDocument() {}
+    public void openDocument() {
+        view.selectHtmlTab(); //выбираем вкладку html
+        JFileChooser fileChooser = new JFileChooser(); //выбор документа
+        fileChooser.setFileFilter(new HTMLFileFilter()); //установить фильтр на выбор документа
+        int choose = fileChooser.showOpenDialog(view);
+        if (choose == JFileChooser.OPEN_DIALOG) { // если открут документ
+            currentFile = fileChooser.getSelectedFile(); //выбираем файл
+            this.resetDocument();
+            view.setTitle(currentFile.getName());
+            // считываем файл
+            try(FileReader reader = new FileReader(currentFile)) {
+                HTMLEditorKit htmlEditorKit = new HTMLEditorKit();
+                htmlEditorKit.read(reader, document, 0);
+                view.resetUndo();
+            } catch (IOException | BadLocationException e) {
+                ExceptionHandler.log(e);
+            }
+        }
+    }
 
-    public void saveDocument() {}
+    public void saveDocument() {
+        view.selectHtmlTab();
+        if (currentFile != null) {
+            try (FileWriter writer = new FileWriter(currentFile)) {
+                HTMLEditorKit htmlEditorKit = new HTMLEditorKit();
+                htmlEditorKit.write(writer, document, 0, document.getLength());
+            } catch (IOException | BadLocationException e) {
+                ExceptionHandler.log(e);
+            }
+        } else {
+            saveDocumentAs();
+        }
+    }
 
     public void saveDocumentAs() {
         view.selectHtmlTab();
@@ -87,9 +115,10 @@ public class Controller {
         if (choose == JFileChooser.APPROVE_OPTION) {
             currentFile = fileChooser.getSelectedFile();
             view.setTitle(currentFile.getName());
-            try (FileWriter writer = new FileWriter(currentFile)) {
+            try (FileWriter writer = new FileWriter(currentFile)){
                 HTMLEditorKit htmlEditorKit = new HTMLEditorKit();
                 htmlEditorKit.write(writer, document, 0, document.getLength());
+
             } catch (IOException | BadLocationException e) {
                 ExceptionHandler.log(e);
             }
